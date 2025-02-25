297f901d3ed433f8c25753e472054959e51cf646
shunechkabon <bondaruk.aleksandra92@gmail.com>
2025-02-25 18:38:16 +0200

    basic styles + add fonts + add svg/png + add data

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
-import App from './components/App';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

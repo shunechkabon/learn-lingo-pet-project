bef0f4c9b12da8dfe6eb9676c29f8a82b902f1e2
shunechkabon <bondaruk.aleksandra92@gmail.com>
2025-03-01 20:36:04 +0200

    update TeachersPage + add TeachersList

--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import TeachersFilters from "../../components/TeachersFilters/TeachersFilters";
 import TeachersList from "../../components/TeachersList/TeachersList";
+import teachersData from "../../data/teachers.json";
 import s from "./TeachersPage.module.css";
 
+const PAGE_SIZE = 4;
+
 const TeachersPage = () => {
+  const [teachers, setTeachers] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(PAGE_SIZE);
+
+  useEffect(() => {
+    setTeachers(teachersData.slice(0, PAGE_SIZE));
+  }, []);
+
+  const handleLoadMore = () => {
+    const newCount = visibleCount + PAGE_SIZE;
+    setTeachers(teachersData.slice(0, newCount));
+    setVisibleCount(newCount);
+  };
+
   return (
     <div className={s.container}>
       <TeachersFilters />
-      <TeachersList />
-      <button>Load more</button>
+      <TeachersList teachers={teachers} />
+      {visibleCount < teachersData.length && (
+        <button onClick={handleLoadMore} className={s.loadMoreBtn}>Load more</button>
+      )}
     </div>
   );
 };
